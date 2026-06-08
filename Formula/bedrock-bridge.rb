a064ce522450263e3291f158ac69ccf698184e5a
Tamirlan Torgayev <10016756+prog893@users.noreply.github.com>
2026-06-08 21:24:16 +0900

    Bump bedrock-bridge to 0.3.0

--- Formula/bedrock-bridge.rb
+++ Formula/bedrock-bridge.rb
@@ -4,9 +4,9 @@ class BedrockBridge < Formula
   desc "Local proxy: Anthropic Messages API to Amazon Bedrock Converse API"
   homepage "https://github.com/prog893/bedrock-bridge"
   url "https://github.com/prog893/bedrock-bridge.git",
-      tag:      "v0.2.0",
-      revision: "241547449890c28bb77d64a8f07a4156d302ef77"
-  version "0.2.0"
+      tag:      "v0.3.0",
+      revision: "3dd3d78a94f935281666b786e8e5d5918cadcd8d"
+  version "0.3.0"
   license "MIT"
 
   depends_on "python@3.13"
